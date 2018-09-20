d58bafdb11b8f84a434eec91c626240e65934559
Becca <bemiller627@gmail.com>
2018-09-20 18:23:36 -0400

    Get listening history from last.fm and find songs listened to on loop

---
+++ consecutive_plays.py
@@ -0,0 +1,87 @@
+""" 
+Create a list of tracks that have been listened to on loop, ordered by date,
+using last.fm listening history
+
+"""
+import pandas as pd
+import os
+
+#Set directory and file name here
+path = 'YOUR_DATA_PATH'
+#You can use my listening history, stored here, or generate your own using lastfm_data
+file_name = 'data/listening_history.csv'
+os.chdir(path) 
+
+#Read in file containing listening history
+history = pd.read_csv(data_path+file_name, encoding='utf-8')
+
+#Generate a list of all songs with >=cutoff subsequent plays
+def looped_tracks(hist,cutoff,repeats=1, chrono=0):
+    """Returns a dataframe containing tracks that have were listened to on repeat,
+    including title, artist, album, date, and number of consecutive plays, in
+    chronological or reverse chronological order.
+
+    Keyword arguments:
+    hist -- dataframe containing listening history
+    cutoff -- the minimum number of consecutive plays to be included
+    repeats -- whether repeats are allowed. If allowed (1), tracks can be included
+               in the dataframe at multiple points. If not (0), only the earliest 
+               instance will be included (default 1)
+    chrono -- Chronological order (1) or reverse chronological order (default 0)
+
+    """
+    
+    count = 0
+    prev = []
+
+    #Add play counts for consecutive tracks in listening history
+    for i in hist.index:
+        if prev==[hist.at[i,'track'], hist.at[i,'artist']]:
+            count = count+1
+        else:
+            prev = [hist.at[i,'track'], hist.at[i,'artist']]
+            count=1
+        hist.at[i,'count'] = count
+    
+    #Filter to only tracks with >=cutoff plays
+    looped = [hist.loc[i] for i in hist.index if hist.at[i,'count']>=cutoff]
+
+    #Sort chronologically, so for repeats, earlier songs are added first
+    looped = looped[::-1]    
+
+    result = [] 
+    tracks = [] 
+    prev = []
+    
+    #Add final song selections to the results list
+    for i in range(0,len(looped)):
+        track = [looped[i].track,looped[i].artist] #current track
+        #Repeats not allowed: only add songs if they're not yet in list
+        if repeats==0 and track not in tracks: 
+            tracks.append(track)
+            result.append(looped[i])
+        #Repeats allowed: add if song is not the same as preceding in list
+        elif repeats==1 and track!=prev:
+            prev = track
+            result.append(looped[i])
+
+    #Reorder to newest first
+    if chrono==0:
+        result = result[::-1]
+
+    #Convert to dataframe and clean
+    result = pd.DataFrame(result)
+    result = result[['track','artist','album','date','count']]  
+        
+    return result
+
+
+#Get list of songs with at least 5 consecutive plays, allowing for repeats
+tracks_rep = looped_tracks(history,cutoff=5,repeats=1,chrono=0)
+tracks_rep = pd.DataFrame(tracks_rep)
+tracks_rep.to_csv('tracks_rep.csv',index=None,encoding='utf-8')
+
+#Similar call, this time disallowing repeats
+tracks_norep = looped_tracks(history,cutoff=5,repeats=0,chrono=0)
+tracks_norep = pd.DataFrame(tracks_norep)
+tracks_norep.to_csv('tracks_norep.csv',index=None,encoding='utf-8')
---
+++ lastfm_data.py
@@ -0,0 +1,76 @@
+""" Download data from last.fm account """
+
+import requests
+import pandas as pd
+
+#Set this to your save path
+save_path = 'YOUR_SAVE_PATH'
+file_name = 'listening_history.csv'
+
+#Rreplace with your key and history
+key = 'YOUR_API_KEY'
+username = 'YOUR_USERNAME'
+
+
+def get_history(username, key,limit=200,page=1):
+    """Returns Last.Fm listening history
+
+    Keyword arguments:
+    username -- last.fm username
+    key -- last.fm API key
+    limit -- the number of tracks to request at once (default 200)
+    page -- the page to start request (default 1)
+
+    """
+    url = 'https://ws.audioscrobbler.com/2.0/?method=user.get{}&user={}&api_key={}&limit={}&page={}&format=json'
+    method = 'recenttracks'
+    request_url = url.format(method, username, key, limit, page)
+    responses = []
+    track_names = []
+    track_mbids = []
+    artist_names = []
+    artist_mbids = []
+    album_names = []
+    album_mbids = []
+    timestamps = []
+    
+    #get the total number of pages
+    request_url = url.format(method, username, key, limit,page)
+    response = requests.get(request_url).json()
+    pages = int(response[method]['@attr']['totalPages'])
+    
+    for page in range(1,pages+1):
+        print('Appending page '+str(page)+' of '+ str(pages))
+        request_url = url.format(method, username, key, limit, page)
+        responses.append(requests.get(request_url))
+    
+    # parse the fields out of each scrobble in each page (aka response) of scrobbles
+    for response in responses:
+        tracks = response.json()
+        for track in tracks[method]['track']:
+            if 'date' in track.keys(): #ie tracking is complete
+                track_names.append(track['name'])
+                track_mbids.append(track['mbid'])
+                artist_names.append(track['artist']['#text'])
+                artist_mbids.append(track['artist']['mbid'])
+                album_names.append(track['album']['#text'])
+                album_mbids.append(track['album']['mbid'])
+                timestamps.append(track['date']['uts'])
+    
+    # create and populate a dataframe to contain the data
+    history = pd.DataFrame()
+    history['track'] = track_names
+    history['mbid'] = track_mbids
+    history['artist'] = artist_names
+    history['artist_mbids'] = artist_mbids
+    history['album'] = album_names
+    history['album_mbids'] = album_mbids
+    history['timestamp'] = timestamps
+    history['datetime'] = pd.to_datetime(history['timestamp'].astype(int), unit='s')
+    history['date']=history['datetime'].dt.date
+    
+    return history
+
+#Function calls to get listening history and save to computer
+history = get_history(username=username,key=key)
+history.to_csv(save_path+file_name, index=None, encoding='utf-8')
